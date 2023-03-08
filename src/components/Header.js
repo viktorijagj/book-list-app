f877fdfd37f4e4498aba728f33e0fd27b003d756
viktorija <viktorija_1102992@yahoo.com>
2023-03-08 23:02:15 +0100

    header changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,13 +5,11 @@ import bookImg from '../img/bookStore.png'
 function Header() {
   return (
     <header className='main-header'>
-      <div className='main-header-logo'>
         <img src={bookImg} alt="Book logo" width={100} height={50}></img>
         <h1 className='main-header-haeding'>BookStore</h1>
-      </div>
-        <nav>
+        {/* <nav>
          <a className='all-books-btn' href="/">Show all Books</a>
-         </nav>
+         </nav> */}
     </header>
   )
 }
